b7370e14fa3af093c316db742d79ed1a7a23f7a5
pravin1989patil <pravin17.patil@gmail.com>
2022-11-14 22:36:32 +0530

    Committed to Temp branch

---
+++ src/test/java/com/spryhealth/assignment/testcases/DemoTestClass2.java
@@ -0,0 +1,13 @@
+package com.spryhealth.assignment.testcases;
+
+import org.testng.annotations.Test;
+
+public class DemoTestClass2 {
+
+	
+	@Test
+	public void  loginTest()
+	{
+		System.out.println("Login Test");
+	}
+}
